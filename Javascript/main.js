89c3571907e72196d19db7c516ed4f99c261eb80
Vivek Kumar <VK285090@GMAIL.COM>
2023-02-02 18:46:43 +0530

    add cdn

---
+++ Javascript/main.js
@@ -0,0 +1,22 @@
+const pdfUrl = '../Docs/DBMS.pdf';
+
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf-render');
+    ctx = canvas.getContext('2d');
+
+
+//Render the page
+ const renderPage = num => {
+    console.log(" ");
+ };  
+
+
+// get Document
+ 
