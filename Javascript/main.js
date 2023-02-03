cd16fb8c563aba34696b27922e9f43117a31723f
Vivek Kumar <VK285090@GMAIL.COM>
2023-02-03 20:33:42 +0530

    completed

--- Javascript/main.js
+++ Javascript/main.js
@@ -12,8 +12,6 @@ const scale = 1.5,
   canvas = document.querySelector("#pdf-render");
 ctx = canvas.getContext("2d");
 
-
-
 //Render the page
 const renderPage = (num) => {
   pageIsRendering = true;
@@ -70,6 +68,19 @@ const showNextPage = () => {
   queueRenderPage(pageNum);
 };
 
+// key press for right and left
+
+function keyPress(e){
+  const key = e.key;
+
+  if (key === "ArrowRight") {
+    showNextPage();
+  }
+  if (key === "ArrowLeft") {
+    showPrevPage();
+  }
+}
+
 // get Document
 pdfjsLib
   .getDocument(pdfUrl)
@@ -99,3 +110,44 @@ document.querySelector("#next-page").addEventListener("click", showNextPage);
 inputPage.addEventListener("input", function (e) {
   renderPage(+inputPage.value);
 });
+
+// step1: get the file from the input element
+inputPdf.addEventListener("change", function (e) {
+  const file = e.target.files[0];
+
+  // step2: Read the file using file reader
+  const fileReader = new FileReader();
+
+  fileReader.onload = function () {
+    // step4: turn array buffer into typed array
+    const typedArray = new Uint8Array(this.result);
+
+    // step5: pdfjs should be able to read this
+    pdfjsLib
+      .getDocument(typedArray)
+      .promise.then((pdfDoc_) => {
+        pdfDoc = pdfDoc_;
+
+        document.querySelector("#page-count").textContent = pdfDoc.numPages;
+
+        renderPage(pageNum);
+      })
+      .catch((error) => {
+        // display error
+        const div = document.createElement("div");
+        div.className = "error";
+        div.appendChild(document.createTextNode(error.message));
+        document.querySelector("body").insertBefore(div, canvas);
+
+        // remove top bar
+        document.querySelector(".top-bar").style.display = "none";
+      });
+  };
+  // step 3: read the file as ArrayBuffer
+  fileReader.readAsArrayBuffer(file);
+});
+
+// key press function called
+document.body.addEventListener("keydown", keyPress);
+
+
